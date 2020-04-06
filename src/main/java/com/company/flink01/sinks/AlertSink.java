4f0e88738fd757f1cbb8f6b0b4ff636c586ba059
JeffRisberg <jrisberg@aol.com>
2020-04-05 18:00:01 -0700

    updated editorconfig

--- src/main/java/com/company/flink01/sinks/AlertSink.java
+++ src/main/java/com/company/flink01/sinks/AlertSink.java
@@ -13,12 +13,12 @@ import org.slf4j.LoggerFactory;
 @SuppressWarnings("unused")
 public class AlertSink implements SinkFunction<Alert> {
 
-    private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-    private static final Logger LOG = LoggerFactory.getLogger(AlertSink.class);
+  private static final Logger LOG = LoggerFactory.getLogger(AlertSink.class);
 
-    @Override
-    public void invoke(Alert value, Context context) {
-        LOG.info(value.toString());
-    }
+  @Override
+  public void invoke(Alert value, Context context) {
+    LOG.info(value.toString());
+  }
 }
--- src/main/java/com/company/flink01/sources/TransactionSource.java
+++ src/main/java/com/company/flink01/sources/TransactionSource.java
@@ -13,35 +13,35 @@ import java.util.Iterator;
 @Public
 public class TransactionSource extends FromIteratorFunction<Transaction> {
 
+  private static final long serialVersionUID = 1L;
+
+  public TransactionSource() {
+    super(new RateLimitedIterator<>(TransactionIterator.unbounded()));
+  }
+
+  private static class RateLimitedIterator<T> implements Iterator<T>, Serializable {
+
     private static final long serialVersionUID = 1L;
 
-    public TransactionSource() {
-        super(new RateLimitedIterator<>(TransactionIterator.unbounded()));
+    private final Iterator<T> inner;
+
+    private RateLimitedIterator(Iterator<T> inner) {
+      this.inner = inner;
     }
 
-    private static class RateLimitedIterator<T> implements Iterator<T>, Serializable {
-
-        private static final long serialVersionUID = 1L;
-
-        private final Iterator<T> inner;
-
-        private RateLimitedIterator(Iterator<T> inner) {
-            this.inner = inner;
-        }
-
-        @Override
-        public boolean hasNext() {
-            return inner.hasNext();
-        }
-
-        @Override
-        public T next() {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            return inner.next();
-        }
+    @Override
+    public boolean hasNext() {
+      return inner.hasNext();
     }
+
+    @Override
+    public T next() {
+      try {
+        Thread.sleep(100);
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+      return inner.next();
+    }
+  }
 }
